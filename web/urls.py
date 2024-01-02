cdf794af3c64db60b8263158327288c622282eec
farhadshakiba1988 <farhadshakiba@yahoo.com>
2024-01-03 00:12:50 +0330

    change index page and add 4 page

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from web import views
 
 urlpatterns = [
-    path('', views.home),
-    path('/index', views.base),
+    path('ui_apps/', views.ui_apps),
+    path('', views.base),
+    path('product_home/', views.product_home),
+    path('ui_products/', views.ui_products),
+    path('products_category/', views.products_category),
 ]
--- web/views.py
+++ web/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
 
-def home(request):
+def ui_apps(request):
     return render(request, 'ui-apps.html')
+
+
+def product_home(request):
+    return render(request, 'ui-app-products-home.html')
 
 
 def base(request):
     return render(request, 'index.html')
+
+
+def products_category(request):
+    return render(request, 'ui-app-products-category.html')
+
+
+def ui_products(request):
+    return render(request, 'ui-app-products-grid2col.html')
